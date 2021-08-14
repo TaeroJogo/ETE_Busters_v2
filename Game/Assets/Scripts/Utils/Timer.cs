1c3befacc901c0f000c263775c1f427ca838e821
LeomaiaJr <leomaiajr@hotmail.com>
2021-08-14 14:31:39 -0300

    usar for ao invés de for each devido a um bug

--- Game/Assets/Scripts/Utils/Timer.cs
+++ Game/Assets/Scripts/Utils/Timer.cs
@@ -4,72 +4,86 @@ using UnityEngine;
 using System;
 
 
-class TimerData {
+class TimerData
+{
 
-  public string timerTag;
+    public string timerTag;
 
-  public float initialTime;
-  public float timeStamp;
-  public float endTime;
+    public float initialTime;
+    public float timeStamp;
+    public float endTime;
 
-  public bool isActive = true;
-  public bool isDone = false;
+    public bool isActive = true;
+    public bool isDone = false;
 
-  public Action timerCallback;
-  public bool loopTimer;
+    public Action timerCallback;
+    public bool loopTimer;
 }
 
 public class Timer : MonoBehaviour
 {
 
-   List<TimerData> timers = new List<TimerData>();
+    List<TimerData> timers = new List<TimerData>();
 
-  void Update(){
-      foreach (var timer in timers)  {
-          if(timer.isActive) {
-            timer.timeStamp -= Time.deltaTime;
-
-            if (timer.timeStamp <= timer.endTime)
+    void Update()
+    {
+        for (var i = 0; i < timers.Count; i++)
+        {
+            if (timers[i].isActive)
             {
-              timer.timerCallback();
-              if(timer.loopTimer) {
-                timer.timeStamp = timer.initialTime;
-              }
-              else {
-                timer.isActive = false;
-              }
+                timers[i].timeStamp -= Time.deltaTime;
+
+                if (timers[i].timeStamp <= timers[i].endTime)
+                {
+                    timers[i].timerCallback();
+                    if (timers[i].loopTimer)
+                    {
+                        timers[i].timeStamp = timers[i].initialTime;
+                    }
+                    else
+                    {
+                        timers[i].isActive = false;
+                    }
+                }
             }
-          }
-      }
+        }
     }
-    
-  public void CreateTimer(string tag, float start, float end, bool loop, Action callbackMethod) {
-    timers.Add(new TimerData {timerTag = tag, initialTime = start, timeStamp = start, endTime = end, timerCallback = callbackMethod, loopTimer = loop});
-  }
 
-  private TimerData FindTimer(string tag){
-    var t = new TimerData();
-    foreach (var timer in timers)  {
-      if(timer.timerTag == tag) {
-        t = timer;
-      }
+    public void CreateTimer(string tag, float start, float end, bool loop, Action callbackMethod)
+    {
+        timers.Add(new TimerData { timerTag = tag, initialTime = start, timeStamp = start, endTime = end, timerCallback = callbackMethod, loopTimer = loop });
     }
-    return t;
-}
 
-  public void PauseTimer(string tag){
-    FindTimer(tag).isActive = false;
-  }
+    private TimerData FindTimer(string tag)
+    {
+        var t = new TimerData();
+        foreach (var timer in timers)
+        {
+            if (timer.timerTag == tag)
+            {
+                t = timer;
+            }
+        }
+        return t;
+    }
 
-  public void ResumeTimer(string tag){
-   FindTimer(tag).isActive = true;
-  }
+    public void PauseTimer(string tag)
+    {
+        FindTimer(tag).isActive = false;
+    }
 
-  public void CancelTimer(string tag){
-    timers.Remove(FindTimer(tag));
-  }
+    public void ResumeTimer(string tag)
+    {
+        FindTimer(tag).isActive = true;
+    }
 
-  public float GetTimeStamp(string tag){
-    return FindTimer(tag).timeStamp;
-  }
+    public void CancelTimer(string tag)
+    {
+        timers.Remove(FindTimer(tag));
+    }
+
+    public float GetTimeStamp(string tag)
+    {
+        return FindTimer(tag).timeStamp;
+    }
 }
