70f924135c60dede64e2496bf15c36a5883f764a
LeomaiaJr <leomaiajr@hotmail.com>
2021-08-16 21:41:25 -0300

    atualizar collider updater para funcionar com CapsuleCollider

--- Game/Assets/Scripts/Player/ColliderUpdater.cs
+++ Game/Assets/Scripts/Player/ColliderUpdater.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class ColliderUpdater : MonoBehaviour
 {
-    
+
+    //make a variable that can be two types, CapsuleCollider2D and BoxCollider2D
+
+
     Sprite currentSprite;
-    BoxCollider2D coll;
+    CapsuleCollider2D capsuleColl;
+    BoxCollider2D boxColl;
     SpriteRenderer spr;
 
-    void Start() {
-        coll = gameObject.GetComponentInChildren<BoxCollider2D>();
+    void Start()
+    {
+        capsuleColl = gameObject.GetComponentInChildren<CapsuleCollider2D>();
+        boxColl = gameObject.GetComponentInChildren<BoxCollider2D>();
+
         spr = gameObject.GetComponentInChildren<SpriteRenderer>();
         UpdateCollider();
     }
 
     void Update()
     {
-	    if(currentSprite != spr.sprite)
-	    {
-		    currentSprite = spr.sprite;
-		    UpdateCollider();
-	    }
+        if (currentSprite != spr.sprite)
+        {
+            currentSprite = spr.sprite;
+            UpdateCollider();
+        }
     }
 
     void UpdateCollider()
     {
-	    coll.size = spr.sprite.bounds.size;
-	    coll.offset = spr.sprite.bounds.center;
-
+        if (capsuleColl != null)
+        {
+            capsuleColl.size = spr.sprite.bounds.size;
+            capsuleColl.offset = spr.sprite.bounds.center;
+        }
+        else if (boxColl != null)
+        {
+            boxColl.size = spr.sprite.bounds.size;
+            boxColl.offset = spr.sprite.bounds.center;
+        }
     }
 }
